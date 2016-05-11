3ca23d87ba11b6c4301a6a10d13e9b85ff86b6b9
Ken Herner <kherner@progress.com>
2016-05-11 18:12:40 -0400

    Move imagemagick to docker-base

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,11 @@
 set -e
 set -x
 
+# Need to set them back for bootstrapping the container.
+TEMP_DIR=/tmp
+TMPDIR=/tmp
+TMP_DIR=/tmp
+
 # Install dependent libraries
 apt-get update && apt-get install -y libssl0.9.8 libsqlite-dev \
   libexpat1 libexpat1-dev libicu-dev libpq-dev libcairo2-dev \
@@ -9,11 +14,3 @@ apt-get update && apt-get install -y libssl0.9.8 libsqlite-dev \
   libmagickcore-dev libmagickwand-dev build-essential libkrb5-dev python-dev \
   libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm \
   ghostscript
-
-# Install ImageMagick
-export MAKEFLAGS="-j $(grep -c ^processor /proc/cpuinfo)"
-cd /opt
-wget http://www.imagemagick.org/download/ImageMagick.tar.gz
-tar -xf ImageMagick.tar.gz && mv ImageMagick-* ImageMagick && cd ImageMagick && ./configure && make && sudo make install
-ldconfig /usr/local/lib && rm -rf /opt/ImageMagick*
-cd /opt
