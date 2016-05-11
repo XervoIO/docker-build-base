59008095c1afe62d636237159de433e93d26c3fb
Ken Herner <kherner@progress.com>
2016-05-11 18:30:22 -0400

    Move image magick back in

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,3 +14,12 @@ apt-get update && apt-get install -y libssl0.9.8 libsqlite-dev \
   libmagickcore-dev libmagickwand-dev build-essential libkrb5-dev python-dev \
   libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm \
   ghostscript
+
+  # Install ImageMagick
+  export MAKEFLAGS="-j $(grep -c ^processor /proc/cpuinfo)"
+  cd /opt
+  wget http://www.imagemagick.org/download/ImageMagick.tar.gz
+  tar -xf ImageMagick.tar.gz && mv ImageMagick-* ImageMagick && cd ImageMagick && ./configure && make && sudo make install
+  # Disable coders that have known vulnerabilities in them
+  sed -i '/<policymap>/a <policy domain="coder" rights="none" pattern="EPHEMERAL" />\n  <policy domain="coder" rights="none" pattern="URL" />\n  <policy domain="coder" rights="none" pattern="HTTPS" />\n  <policy domain="coder" rights="none" pattern="MVG" />\n  <policy domain="coder" rights="none" pattern="MSL" />\n  <policy domain="coder" rights="none" pattern="TEXT" />\n  <policy domain="coder" rights="none" pattern="SHOW" />\n  <policy domain="coder" rights="none" pattern="WIN" />\n  <policy domain="coder" rights="none" pattern="PLT" />\n' /usr/local/etc/ImageMagick-7/policy.xml
+  ldconfig /usr/local/lib && rm -rf /opt/ImageMagick*
